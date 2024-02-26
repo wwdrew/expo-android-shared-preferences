597066d7e77b90594b59b989e981913dbea9f9a4
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-02-26 14:18:56 +0000

    feat: allow passing filename to get and set functions

--- android/src/main/java/expo/modules/androidsharedpreferences/ExpoAndroidSharedPreferencesModule.kt
+++ android/src/main/java/expo/modules/androidsharedpreferences/ExpoAndroidSharedPreferencesModule.kt
@@ -11,12 +11,12 @@ class ExpoAndroidSharedPreferencesModule : Module() {
   private val context
     get() = requireNotNull(appContext.reactContext)
 
-  private val sharedPreferences by lazy {
-    context.getSharedPreferences("${context.packageName}_preferences", Context.MODE_PRIVATE)
+  private fun sharedPreferences(filename: String? = null): SharedPreferences {
+    val file = filename ?: "${context.packageName}_preferences"
+    return context.getSharedPreferences(file, Context.MODE_PRIVATE)
   }
-
   private val preferenceChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
-    val value = sharedPreferences.getString(key, null)
+    val value = sharedPreferences().getString(key, null)
 
     sendEvent("onChange", mapOf(
       "key" to key,
@@ -30,15 +30,15 @@ class ExpoAndroidSharedPreferencesModule : Module() {
     Events("onChange")
 
     OnCreate {
-      sharedPreferences.registerOnSharedPreferenceChangeListener(preferenceChangeListener)
+      sharedPreferences().registerOnSharedPreferenceChangeListener(preferenceChangeListener)
     }
 
-    Function("get") { key: String ->
-      sharedPreferences.getString(key, null)
+    Function("get") { key: String, filename: String? ->
+      sharedPreferences(filename).getString(key, null)
     }
 
-    Function("set") { key: String, value: String ->
-      sharedPreferences.edit().putString(key, value).apply()
+    Function("set") { key: String, value: String, filename: String? ->
+      sharedPreferences(filename).edit().putString(key, value).apply()
     }
   }
 }
