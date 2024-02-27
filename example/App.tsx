73485b38b2b1d474a5e6d3379c8fc04d135a1378
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-02-27 12:01:34 +0000

    add remaining functions

--- example/App.tsx
+++ example/App.tsx
@@ -1,24 +1,72 @@
-import { useSharedPreferences } from "expo-android-shared-preferences";
+import {
+  getBoolean,
+  getFloat,
+  getInt,
+  getLong,
+  getString,
+  setBoolean,
+  setFloat,
+  setInt,
+  setLong,
+} from "expo-android-shared-preferences";
+import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [keyValue, setKeyValue] = useSharedPreferences("key");
-  const [otherValue, setOtherValue] = useSharedPreferences("other");
+  const [stringValue, setStringValue] = useState(
+    getString("string", "initialString"),
+  );
+  const [intValue, setIntValue] = useState(getInt("intValue", 1234));
+  const [floatValue, setFloatValue] = useState(getFloat("floatValue", 12.34));
+  const [longValue, setLongValue] = useState(
+    getLong("longValue", 54836785364878),
+  );
+  const [booleanValue, setBooleanValue] = useState(
+    getBoolean("booleanValue", true),
+  );
 
-  function handleSetKeyPress() {
-    setKeyValue(Math.random().toString().substring(2, 8));
+  function handleSetStringPress() {
+    setStringValue(Math.random().toString().substring(2, 8));
   }
 
-  function handleSetOtherPress() {
-    setOtherValue(Math.random().toString().substring(2, 8));
+  function handleSetIntPress() {
+    const random = Number(Math.random().toString().substring(2, 4));
+
+    setIntValue(random);
+    setInt("intValue", random);
+  }
+
+  function handleSetLongPress() {
+    const random = Number(Math.random().toString().substring(2, 10));
+
+    setLongValue(random);
+    setLong("longValue", random);
+  }
+
+  function handleSetFloatPress() {
+    const random = Math.random();
+
+    setFloatValue(random);
+    setFloat("floatValue", random);
+  }
+
+  function handleSetBooleanPress() {
+    setBoolean("booleanValue", !booleanValue);
+    setBooleanValue(!booleanValue);
   }
 
   return (
     <View style={styles.container}>
-      <Button onPress={handleSetKeyPress} title="Set" />
-      <Text>"key" value: {keyValue}</Text>
-      <Button onPress={handleSetOtherPress} title="Set" />
-      <Text>"other" value: {otherValue}</Text>
+      <Button onPress={handleSetStringPress} title="Set String" />
+      <Text>String: {stringValue}</Text>
+      <Button onPress={handleSetIntPress} title="Set Int" />
+      <Text>Int: {intValue}</Text>
+      <Button onPress={handleSetLongPress} title="Set Long" />
+      <Text>Long: {longValue}</Text>
+      <Button onPress={handleSetFloatPress} title="Set Float" />
+      <Text>Float: {floatValue}</Text>
+      <Button onPress={handleSetBooleanPress} title="Set Boolean" />
+      <Text>Boolean: {booleanValue ? "true" : "false"}</Text>
     </View>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,101 @@ import ExpoAndroidSharedPreferencesModule from "./ExpoAndroidSharedPreferencesMo
 
 export { useSharedPreferences } from "./useSharedPreferences";
 
-export function get(key: string, filename?: string): string {
-  return ExpoAndroidSharedPreferencesModule.get(key, filename);
+export function contains(key: string, filename?: string): boolean {
+  return ExpoAndroidSharedPreferencesModule.contains(key, filename);
 }
 
-export function set(key: string, value: string, filename?: string): void {
-  ExpoAndroidSharedPreferencesModule.set(key, value, filename);
+export function remove(key: string, filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.remove(key, filename);
+}
+
+export function clear(filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.clear(filename);
+}
+
+// String
+export function getString(
+  key: string,
+  defaultValue?: string,
+  filename?: string,
+): string {
+  return ExpoAndroidSharedPreferencesModule.getString(
+    key,
+    defaultValue,
+    filename,
+  );
+}
+
+export function setString(key: string, value: string, filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.setString(key, value, filename);
+}
+
+// Int
+export function getInt(
+  key: string,
+  defaultValue?: number,
+  filename?: string,
+): number {
+  return ExpoAndroidSharedPreferencesModule.getInt(key, defaultValue, filename);
+}
+
+export function setInt(key: string, value: number, filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.setInt(key, value, filename);
+}
+
+// Long
+export function getLong(
+  key: string,
+  defaultValue?: number,
+  filename?: string,
+): number {
+  return ExpoAndroidSharedPreferencesModule.getLong(
+    key,
+    defaultValue,
+    filename,
+  );
+}
+
+export function setLong(key: string, value: number, filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.setLong(key, value, filename);
+}
+
+// Float
+export function getFloat(
+  key: string,
+  defaultValue?: number,
+  filename?: string,
+): number {
+  return ExpoAndroidSharedPreferencesModule.getFloat(
+    key,
+    defaultValue,
+    filename,
+  );
+}
+
+export function setFloat(key: string, value: number, filename?: string): void {
+  ExpoAndroidSharedPreferencesModule.setFloat(key, value, filename);
+}
+
+// Boolean
+export function getBoolean(
+  key: string,
+  defaultValue?: boolean,
+  filename?: string,
+): boolean {
+  return ExpoAndroidSharedPreferencesModule.getBoolean(
+    key,
+    defaultValue,
+    filename,
+  );
+}
+
+export function setBoolean(
+  key: string,
+  value: boolean,
+  filename?: string,
+): void {
+  ExpoAndroidSharedPreferencesModule.setBoolean(key, value, filename);
 }
 
 const emitter = new EventEmitter(ExpoAndroidSharedPreferencesModule);
